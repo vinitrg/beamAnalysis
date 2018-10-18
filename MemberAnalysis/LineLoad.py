2126e08e93185f718963ab79e1709ae666fee469
vinitrg <vinit.gujarathi@gmail.com>
2018-10-18 21:14:52 +0530

    Adding Distributed Load class

---
+++ MemberAnalysis/LineLoad.py
@@ -0,0 +1,6 @@
+class LineLoads:
+	def __init__(self, pointA, pointB,intensityAtPointA, intensityAtPointB):
+		self.PointA = pointA
+		self.PointB = pointB
+		self.IntensityAtPointA = intensityAtPointA
+		self.IntensityAtPointB = intensityAtPointB
